123d3defc4f38cddb3f266823d27805056fbb93d
Ajay Patel <patel.ajay285@gmail.com>
2019-01-09 15:31:15 -0800

    Add fib.py for benchmarking

---
+++ src/examples/fib.py
@@ -0,0 +1,60 @@
+# This program implements calculating the n-th term of the Fibonacci sequence using three ways. 
+# It is useful for quickly benchmarking and understanding how much slower Coldbrew is than a 
+# native CPython interpreter.
+#
+# There are 3 Fibonacci implementations that test various types of workloads
+# 1) Recursive Fibonacci - Helps tests workloads that are that are CPU bound and function call heavy
+# 2) Iterative Fibonacci - Helps tests workloads that are CPU bound and not function call heavy
+# 3) FileIO Iterative Fibonacci - Helps tests workloads that are IO bound
+
+import argparse
+import json
+import os
+import tempfile
+import time
+
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n-2) + fib(n-1)
+
+def fibi(n):
+    a, b = 0, 1
+    for i in range(0, n):
+        a, b = b, a + b
+    return a
+
+def fibf(n):
+    tempf = os.path.join(tempfile.gettempdir(), 'fib.json')
+    a, b = 0, 1
+    with open(tempf, 'w+') as fp:
+        json.dump((a, b), fp)
+    for i in range(0, n):
+        with open(tempf, 'r') as fp:
+            a, b = json.load(fp)
+        a, b = b, a + b
+        with open(tempf, 'w+') as fp:
+            json.dump((a, b), fp)
+    return a
+
+
+parser = argparse.ArgumentParser(description="calculates fib(N)")
+parser.add_argument("n", type=int, help="up to what term to calculate the fibonacci sequence to")
+parser.add_argument("-v", "--verbose", action="store_true")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-i", "--iterative", action="store_true")
+group.add_argument("-f", "--fileio", action="store_true")
+args = parser.parse_args()
+if args.iterative:
+    fib_func = fibi
+elif args.fileio:
+    fib_func = fibf
+else:
+    fib_func = fib
+btime = time.time()
+answer = fib_func(args.n)
+etime = time.time()
+if args.verbose:
+    print("fib(%d) = %d took %f seconds to calculate" % (args.n, answer, etime-btime))
+else:
+    print(answer)
--- src/slim_data_bundle.py
+++ src/slim_data_bundle.py
@@ -5,6 +5,8 @@ import os
 oldcwd = os.getcwd()
 
 paths = []
+pycpaths = ['/usr/local/lib/python*/encodings/*.pyc', '/usr/local/lib/python*/encodings/**/.pyc']
+
 with open('../customize/keeplist.txt', 'r') as inf:
     for line in inf:
         line = line.strip()
@@ -26,6 +28,12 @@ for globpath in glob.iglob(r'./usr/local/lib/python*/**/*', recursive=True):
             if fnmatch.fnmatch(globpath, path):
                 should_keep = True
                 break
+        if globpath.endswith('.pyc'):
+            should_keep = False
+            for path in pycpaths:
+                if fnmatch.fnmatch(globpath, path):
+                    should_keep = True
+                    break
         if not should_keep:
             print("Removing to slim data bundle:", globpath[1:])
             os.remove(globpath[1:])
