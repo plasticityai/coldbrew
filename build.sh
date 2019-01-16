bce7c37f2dba8f6c7f5b20ab46b4688668447ecf
Ajay Patel <patel.ajay285@gmail.com>
2019-01-15 23:38:55 -0800

    Debug

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 
 docker build . -t coldbrew:latest
 mkdir -p $(pwd)/cache
+echo docker run --rm -t -v $(pwd):/BUILD -v $(pwd)/cache:/root/.emscripten_cache/ coldbrew:latest
+ls $(pwd)/src
 docker run --rm -t -v $(pwd):/BUILD -v $(pwd)/cache:/root/.emscripten_cache/ coldbrew:latest
