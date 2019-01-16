03ee98eaf72a4f8f56ceced9c01403188bb9c839
Ajay Patel <patel.ajay285@gmail.com>
2019-01-15 16:24:38 -0800

    Remove -i from build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 docker build . -t coldbrew:latest
 mkdir -p $(pwd)/cache
-docker run --rm -it -v $(pwd):/BUILD -v $(pwd)/cache:/root/.emscripten_cache/ coldbrew:latest
+docker run --rm -t -v $(pwd):/BUILD -v $(pwd)/cache:/root/.emscripten_cache/ coldbrew:latest
