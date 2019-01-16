9a3fae07e2897db42577cf0f38e20d9f14d0c4f3
Ajay Patel <patel.ajay285@gmail.com>
2019-01-15 23:53:33 -0800

    Build in original directory

--- build.sh
+++ build.sh
@@ -2,6 +2,4 @@
 
 docker build . -t coldbrew:latest
 mkdir -p $(pwd)/cache
-echo docker run --rm -t -v $(pwd):/BUILD -v $(pwd)/cache:/root/.emscripten_cache/ coldbrew:latest
-ls $(pwd)/src
 docker run --rm -t -v $(pwd):/BUILD -v $(pwd)/cache:/root/.emscripten_cache/ coldbrew:latest
